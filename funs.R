ccf3dea58d8aa31d35aef9405218bd6ed47efef0
Matt Lyon <ml18692@bristol.ac.uk>
2022-02-07 12:20:27 +0000

    added sim

--- funs.R
+++ funs.R
@@ -1,3 +1,7 @@
+library("dplyr")
+library("broom")
+library("varGWASR")
+
 #' Function to simulate genotypes in HWE
 #' @param q Recessive/alternative allele frequency
 #' @param n_obs Number of observations to return
@@ -5,4 +9,37 @@ get_simulated_genotypes <- function(q, n_obs){
   p <- 1 - q
   x <- sample(c(0, 1, 2), n_obs, prob=c(p^2, 2 * p * q, q^2), replace=T)
   return(x)
+}
+
+get_est <- function(z, x, y){
+    exp_fit <- lm(x ~ z)
+    b_exp <- exp_fit %>% tidy %>% dplyr::filter(term == "z") %>% dplyr::select("estimate") %>% as.numeric
+    f_exp <- summary(exp_fit)$fstatistic[['value']]
+    b_out <- lm(y ~ z) %>% tidy %>% dplyr::filter(term == "z") %>% dplyr::select("estimate") %>% as.numeric
+    b_mr <- b_out/b_exp
+    b_obs <- lm(y ~ x) %>% tidy %>% dplyr::filter(term == "x") %>% dplyr::select("estimate") %>% as.numeric
+    fit1 <- varGWASR::model(data.frame(z,x), "z", "x")
+    names(fit1) <- paste0(names(fit1), ".zx")
+    fit2 <- varGWASR::model(data.frame(z,y), "z", "y")
+    names(fit2) <- paste0(names(fit2), ".zy")
+    fit <- cbind(fit1, fit2)
+    fit$b_mr <- b_mr
+    fit$b_obs <- b_obs
+    fit$lin_var <- var(x[z==2]) - var(x[z==0])
+    fit$f_exp <- f_exp
+    return(fit)
+}
+
+get_plot <- function(results){
+  results$iv1 <- results$f_exp > 10
+  p <- ggplot(results, aes(x=-log10(phi_p), y=b_mr, color=iv1)) +
+    geom_point(alpha=0.7) +
+    facet_grid(~ pb, scale="free_x") +
+    scale_colour_grey(start = 0.8,end = 0.2) +
+    labs(x="Variance test -log10(P)", y="MR point estimate", color="IV1 (F>10)") +
+    theme_classic() +
+    scale_x_continuous(breaks=scales::pretty_breaks()) +
+    geom_hline(yintercept = mean(b_mr), color="grey", linetype="dashed") +
+    theme(legend.position="bottom", legend.box.background = element_rect(colour = "black"))
+    return(p)
 }
---
+++ sim5.R
@@ -0,0 +1,109 @@
+library("dplyr")
+library("broom")
+library("varGWASR")
+library("ggplot2")
+source("funs.R")
+set.seed(123)
+
+n_sim <- 1000
+n_obs <- 10000
+
+# Z-X homogeneity
+# X-Y homogeneity
+results1 <- data.frame()
+for (pb in seq(0.5, 0.5)){
+  for (i in 1:n_sim){
+      c <- rnorm(n_obs)
+      z <- get_simulated_genotypes(0.25, n_obs)
+      u <- rbinom(n_obs, 1, pb)
+      x <- z + c + rnorm(n_obs)
+      y <- x + c + rnorm(n_obs)
+      result <- get_est(z, x, y)
+      result$pb <- pb
+      results1 <- rbind(results1, result)
+  }
+}
+
+# Z-X heterogeneity i.e. sex-specific effects of SNP-exposure relationship (eg SLC2A9 x sex on urate)
+# X-Y homogeneity (which is considered implausible)
+# Z has variance effect on X and Y but is underpowered to detect Z-Y heterogeneity
+results2 <- data.frame()
+for (pb in seq(0.5, 0.5)){
+  for (i in 1:n_sim){
+      c <- rnorm(n_obs)
+      z <- get_simulated_genotypes(0.25, n_obs)
+      u <- rbinom(n_obs, 1, pb)
+      x <- z + z*u + c + rnorm(n_obs)
+      y <- x + c + rnorm(n_obs)
+      result <- get_est(z, x, y)
+      result$pb <- pb
+      results2 <- rbind(results2, result)
+  }
+}
+
+# Z-X homogeneity (is plausible)
+# X-Y heterogeneity (is plausible)
+# Z has no variance effect on X but does on Y but is underpowered to detect Z-Y heterogeneity
+results3 <- data.frame()
+for (pb in seq(0.5, 0.5)){
+  for (i in 1:n_sim){
+      c <- rnorm(n_obs)
+      z <- get_simulated_genotypes(0.25, n_obs)
+      u <- rbinom(n_obs, 1, pb)
+      x <- z + c + rnorm(n_obs)
+      y <- x + x*u + c + rnorm(n_obs)
+      result <- get_est(z, x, y)
+      result$pb <- pb
+      results3 <- rbind(results3, result)
+  }
+}
+
+# Z-X heterogeneity (is plausible)
+# X-Y heterogeneity (is plausible)
+# Modifier is the same
+# Z-X and Z-Y are strong variance effects
+results4 <- data.frame()
+for (pb in seq(0.5, 0.5)){
+  for (i in 1:n_sim){
+      c <- rnorm(n_obs)
+      z <- get_simulated_genotypes(0.25, n_obs)
+      u <- rbinom(n_obs, 1, pb)
+      x <- z + z*u + c + rnorm(n_obs)
+      y <- x + x*u + c + rnorm(n_obs)
+      result <- get_est(z, x, y)
+      result$pb <- pb
+      results4 <- rbind(results4, result)
+  }
+}
+
+# Z-X heterogeneity (is plausible)
+# X-Y heterogeneity (is plausible)
+# Modifier is not the same
+results5 <- data.frame()
+for (pb in seq(0.5, 0.5)){
+  for (i in 1:n_sim){
+      c <- rnorm(n_obs)
+      z <- get_simulated_genotypes(0.25, n_obs)
+      u1 <- rbinom(n_obs, 1, pb)
+      u2 <- rbinom(n_obs, 1, pb)
+      x <- z + z*u1 + c + rnorm(n_obs)
+      y <- x + x*u2 + c + rnorm(n_obs)
+      result <- get_est(z, x, y)
+      result$pb <- pb
+      results5 <- rbind(results5, result)
+  }
+}
+
+# combine results
+results1$sim <- 1
+results2$sim <- 2
+results3$sim <- 3
+results4$sim <- 4
+results5$sim <- 5
+results <- rbind(
+    results1,results2,results3,results4,results5
+)
+
+# plots
+#qqPlot(results$phi_p.zx)
+#qqPlot(results$phi_p.zy)
