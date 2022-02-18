56428b43a7e21e80caa3fdf08755d709a97c7900
Matt Lyon <ml18692@bristol.ac.uk>
2022-02-18 10:36:33 +0000

    added sim9

--- sim9.R
+++ sim9.R
@@ -4,20 +4,22 @@
 
 library("dplyr")
 library("ggplot2")
+library("grid")
+library("gtable")
 library("TwoSampleMR")
 library("viridis")
 source("funs.R")
 set.seed(123)
 
-n_sim <- 50
-r2_u <- 0.05
-r2_x <- 0.05
-r2_xu <- r2_x * 0.05
+n_sim <- 5
+r2_u <- 0.05 # U-Y main effect
+r2_x <- 0.05 # X-Y main effect
+r2_xu <- r2_x * 0.5 # X-Y interaction effect half the size of the main effect
 
 results <- data.frame()
-for (n_obs in c(100000)){
-    for (r2_z in seq(0.01, 0.05, 0.01)){
-        for (phi in seq(0, 6, 0.5)){
+for (n_obs in c(1000, 10000, 100000)){
+    for (r2_z in seq(0.01, 0.05, 0.01)){ # variance explained by Z-X
+        for (phi in seq(0, 2.5, 0.5)){ # size of Z-X interaction effect relative to main effect
             r2_zu <- r2_z * phi
             u_b <- sqrt(r2_u)
             z_b <- sqrt(r2_z)
@@ -51,7 +53,7 @@ for (n_obs in c(100000)){
                 result$b_mr <- wald$b
                 result$b <- sqrt(r2_x)
                 result$phi <- phi
-                result$bias <- (result$b_mr - result$b) / result$b
+                result$bias <- result$b_mr / result$b
 
                 results <- rbind(results, result)
             }
@@ -60,4 +62,60 @@ for (n_obs in c(100000)){
 }
 
 # power
-results %>% dplyr::group_by(n_obs, r2_z, phi) %>% dplyr::summarize(binom.test(sum(phi_p < 0.05), n()) %>% tidy)
+pwr <- results %>% dplyr::group_by(n_obs, r2_z, phi) %>% dplyr::summarize(cbind(binom.test(sum(phi_p < 0.05), n()) %>% tidy, bias=mean(bias)))
+
+# plot
+pwr$n_obs <- as.factor(pwr$n_obs)
+pwr$n_obs <- levels(pwr$n_obs) <- c("1000", "10,000", "100,000")
+p <- ggplot(pwr, aes(x=n_obs, y=estimate, ymin=conf.low, ymax=conf.high, color=bias)) +
+    geom_line() + 
+    geom_point() + 
+    geom_errorbar() +
+    theme_classic() + 
+    labs(x = "Sample size", y = "Power (\u03b1=5%)", color = "Fold-change") +
+    scale_y_continuous(limits = c(0, 1), breaks = c(0,.25,.5,.75,1)) +
+    geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
+    geom_hline(yintercept = 0.05, linetype = "dashed", color = "grey") +
+    scale_color_viridis(direction = 1) +
+    facet_grid(phi~r2_z) +
+    ggtitle("Variance explained by Z-X") +
+    theme(
+        strip.background = element_blank(),
+        legend.position = "bottom",
+        legend.background = element_blank(),
+        legend.box.background = element_rect(colour = "black"),
+        panel.spacing = unit(1, "lines"), 
+        axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1),
+        plot.title = element_text(hjust = 0.5, size=11)
+    )
+
+# text, size, colour for added text
+text = "Size of Z-X interaction effect relative to main effect"
+size = 11
+col = "black"
+
+# Convert the plot to a grob
+gt <- ggplotGrob(p)
+
+# Get the positions of the right strips in the layout: t = top, l = left, ...
+strip <-c(subset(gt$layout, grepl("strip-r", gt$layout$name), select = t:r))
+
+# Text grob
+text.grob = textGrob(text, rot = -90,
+   gp = gpar(fontsize = size, col = col))
+
+# New column to the right of current strip
+# Adjusts its width to text size
+width = unit(2, "grobwidth", text.grob) + unit(1, "lines")
+gt <- gtable_add_cols(gt, width, max(strip$r))  
+
+# Add text grob to new column
+gt <- gtable_add_grob(gt, text.grob, 
+        t = min(strip$t), l = max(strip$r) + 1, b = max(strip$b))
+
+# Draw it
+grid.newpage()
+
+pdf("sim9.pdf")
+grid.draw(gt)
+dev.off()
