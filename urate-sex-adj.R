7e25c0d5eda4041efb43fceda27d6093d9509371
Matt Lyon <ml18692@bristol.ac.uk>
2022-02-25 14:53:52 +0000

    added urate by-sex analsyis

--- urate-sex-adj.R
+++ urate-sex-adj.R
@@ -32,6 +32,7 @@ pc <- get_genetic_principal_components()
 # merge data
 dat <- merge(linker, pc, "appieu")
 dat <- merge(dat, pheno, by.x="app15825", by.y="eid")
+dat$urate.30880.0.0 <- dat$urate.30880.0.0 / sd(dat$urate.30880.0.0, na.rm=T)
 
 # select SNPs and extract from UKBB
 for (i in 1:nrow(exposure_dat)){
@@ -51,10 +52,21 @@ for (i in 1:nrow(exposure_dat)){
 # test for IV effect on urate variance w/wo adjusting for by-sex effect
 results <- data.frame()
 for (i in 1:nrow(exposure_dat)){
+    # define SNP & int term
+    snp <- paste0("chr", exposure_dat$chr.exposure[i], "_", as.double(exposure_dat$pos.exposure[i]), "_", exposure_dat$other_allele.exposure[i], "_", exposure_dat$effect_allele.exposure[i])
+    dat$gxs <- dat[[snp]] * dat$sex.31.0.0
     # fit wo by-sex effect
-    fit0 <- varGWASR::model(data.frame(x, y), "x", "y")
+    covar <- c("age_at_recruitment.21022.0.0", "sex.31.0.0", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", "PC7", "PC8", "PC9", "PC10")
+    fit0 <- varGWASR::model(dat %>% dplyr::select(!!covar, snp, "urate.30880.0.0") %>% tidyr::drop_na(.), snp, "urate.30880.0.0", covar1 = covar, covar2 = covar)
     # fit w by-sex effect
-
+    covar <- c(covar, "gxs")
+    fit1 <- varGWASR::model(dat %>% dplyr::select(!!covar, snp, "urate.30880.0.0") %>% tidyr::drop_na(.), snp, "urate.30880.0.0", covar1 = covar, covar2 = covar)
+    # store result
+    names(fit0) <- paste0(names(fit0), ".m")
+    names(fit1) <- paste0(names(fit1), ".i")
+    result <- cbind(fit0, fit1)
+    result$snp <- snp
+    results <- rbind(results, result)
 }
 
 # plot -log10(P) variance effect w/wo adjusting for by-sex effect
