7e253c9c666f8831cca6cb3b31312f0da8b4ca6c
Matt Lyon <ml18692@bristol.ac.uk>
2022-04-05 14:26:44 +0100

    added sim

--- sim11.R
+++ sim11.R
@@ -7,15 +7,11 @@ library("viridis")
 source("funs.R")
 set.seed(123)
 
-n_sim <- 30
-r2_u <- 0.05 # U-Y main effect
-r2_x <- 0.05 # X-Y main effect
-r2_xu <- r2_x * 0.5 # X-Y interaction effect half the size of the main effect
-
-# log OR effects
-b1 <- log(1.2) # causal effect of X-Y
-b2 <- log(1.2) # causal effect of U-Y
-b3 <- b1 * .5 # X-Y interaction effect half the size of the main effect
+n_sim <- 5
+r2_u <- 0.05 # U-X main effect
+x_b <- log(1.2) # X-Y main effect
+u_b <- log(1.2) # U-Y main effect
+xu_b <- x_b * 0.5 # X*U-Y effect half the size of the main effect
 
 results <- data.frame()
 for (n_obs in c(10000, 50000, 100000)){
@@ -25,16 +21,14 @@ for (n_obs in c(10000, 50000, 100000)){
             u_b <- sqrt(r2_u)
             z_b <- sqrt(r2_z)
             zu_b <- sqrt(r2_zu)
-            xu_b <- sqrt(r2_xu)
             for (i in 1:n_sim){
                 # simulate variables
                 z <- get_simulated_genotypes(0.25, n_obs); z <- scale(z)
-                u <- rbinom(n_obs, 1, 0.5); u <- scale(u)
+                u <- rnorm(n_obs)
                 x <- z*z_b + u*u_b + z*u*zu_b + rnorm(n_obs, sd=sqrt(1-(r2_z+r2_u+r2_zu))); x <- scale(x)
-                varX <- var(x)
 
                 # simulate binary outcome
-                p <- x*b1 + u*b2 + x*u*b3
+                p <- x*x_b + u*u_b + x*u*xu_b
                 p <- 1/(1+exp(-p))
                 y <- rbinom(n_obs, 1, p)
 
@@ -52,12 +46,11 @@ for (n_obs in c(10000, 50000, 100000)){
 
                 # store result
                 result$n_obs <- n_obs
-                result$varX <- varX
                 result$r2_z <- r2_z
                 result$r2_zu <- r2_zu
-                result$r2_xu <- r2_xu
+                result$xu_b <- xu_b
                 result$b_mr <- wald$b
-                result$b <- b1
+                result$b <- x_b
                 result$phi <- phi
                 result$bias <- result$b_mr / result$b
 
@@ -68,7 +61,7 @@ for (n_obs in c(10000, 50000, 100000)){
 }
 
 # save data
-write.table(file="sim9.txt", results)
+write.table(file="data/sim11.txt", results)
 
 # estimate power
 pwr <- results %>% 
@@ -88,7 +81,7 @@ p <- ggplot(pwr, aes(x=n_obs, y=estimate, ymin=conf.low, ymax=conf.high, color=b
     geom_hline(yintercept = 0.8, linetype = "dashed", color = "grey") +
     geom_hline(yintercept = 0.05, linetype = "dashed", color = "grey") +
     scale_color_viridis(direction = 1) +
-    scale_x_discrete(labels = c('1000','10,000','50,000')) +
+    scale_x_discrete(labels = c('10,000','50,000','100,000')) +
     facet_grid(phi~r2_z) +
     ggtitle("Instrument-exposure main effect variance explained") +
     theme(
@@ -124,7 +117,7 @@ gt <- gtable::gtable_add_cols(gt, width, max(strip$r))
 gt <- gtable::gtable_add_grob(gt, text.grob, 
         t = min(strip$t), l = max(strip$r) + 1, b = max(strip$b))
 
-pdf("sim9.pdf")
+pdf("sim11.pdf")
 # Draw it
 grid.newpage()
 grid.draw(gt)
