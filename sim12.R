3df96064c157dedbc738adf9449f35132405c110
Matt Lyon <ml18692@bristol.ac.uk>
2022-04-12 15:11:51 +0100

    added stratified analsyis to sim12

--- sim12.R
+++ sim12.R
@@ -7,7 +7,25 @@ library("viridis")
 source("funs.R")
 set.seed(123)
 
-n_sim <- 30
+mr <- function(b_exp, b_out, se_exp, se_out, phi_p, n_obs, q){
+    # define P-threshold using quantile
+    p_var <- quantile(phi_p, q)
+
+    # Drop top q SNPs with IV-exp variance effect
+    b_exp <- b_exp[phi_p >= p_var]
+    b_out <- b_out[phi_p >= p_var]
+    se_exp <- se_exp[phi_p >= p_var]
+    se_out <- se_out[phi_p >= p_var]
+
+    # IVW
+    ivw <- mr_ivw(b_exp, b_out, se_exp, se_out, NULL)
+    ivw$q <- q
+    ivw$n_obs <- n_obs
+
+    return(ivw)
+}
+
+n_sim <- 10
 r2_u <- 0.2 # U-Y main effect (small Cohen d)
 r2_x <- 0.2 # X-Y main effect (small Cohen d)
 r2_xu <- r2_x * 0.5 # X-Y interaction effect half the size of the main effect
@@ -21,18 +39,19 @@ u_b <- sqrt(r2_u)
 xu_b <- sqrt(r2_xu)
 
 results <- data.frame()
+f_stats <- data.frame()
 for (n_obs in c(5000,10000,100000)){
     for (i in 1:n_sim){
         # select SNP betas
         if (n_obs == 5000){
             z_b <- rnorm(100000, sd=0.025)
-            z_b <- sample(z_b[abs(z_b) > 0.08], n_snps)
+            z_b <- sample(z_b[abs(z_b) > 0.085], n_snps)
         } else if (n_obs == 10000){
             z_b <- rnorm(100000, sd=0.025)
             z_b <- sample(z_b[abs(z_b) > 0.06], n_snps)
         } else if (n_obs == 100000){
             z_b <- rnorm(100000, sd=0.025)
-            z_b <- sample(z_b[abs(z_b) > 0.02], n_snps)
+            z_b <- sample(z_b[abs(z_b) > 0.005], n_snps)
         }
         
         # set size of interaction effect relative to main effect
@@ -51,24 +70,28 @@ for (n_obs in c(5000,10000,100000)){
         se_exp <- sapply(1:n_snps, function(i) lm(x~z[,i]) %>% tidy %>% dplyr::filter(term!="(Intercept)") %>% dplyr::pull(std.error))
         b_out <- sapply(1:n_snps, function(i) lm(y~z[,i]) %>% tidy %>% dplyr::filter(term!="(Intercept)") %>% dplyr::pull(estimate))
         se_out <- sapply(1:n_snps, function(i) lm(y~z[,i]) %>% tidy %>% dplyr::filter(term!="(Intercept)") %>% dplyr::pull(std.error))
-        f_stat <- sapply(1:n_snps, function(i) summary(lm(x~z[,i]))$fstatistic[1] %>% as.numeric)
+        f_stat <- sapply(n_isnps+1:(n_snps-n_isnps), function(i) summary(lm(x~z[,i]))$fstatistic[1] %>% as.numeric)
         phi_p <- sapply(1:n_snps, function(i) model(data.frame(z, x), paste0("X", i), "x")$phi_p)
 
-        # IVW with all SNPs
-        mr_all <- mr_ivw(b_exp, b_out, se_exp, se_out, NULL)
-        mr_all_b <- mr_all$b
-        mr_all_p <- mr_all$pval
+        # estimate IVW with subsets of instruemnts
+        results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, n_obs, 0.75))
+        results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, n_obs, 0.5))
+        results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, n_obs, 0.25))
+        results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, n_obs, 0.1))
+        results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, n_obs, 0.05))
+        results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, n_obs, 0))
 
-        # IVW with homogeneous subset
-        b_exp <- b_exp[phi_p > p_thresh]
-        b_out <- b_out[phi_p > p_thresh]
-        se_exp <- se_exp[phi_p > p_thresh]
-        se_out <- se_out[phi_p > p_thresh]
-        mr_homo <- mr_ivw(b_exp, b_out, se_exp, se_out, NULL)
-        mr_homo_b <- mr_homo$b
-        mr_homo_p <- mr_homo$pval
-
-        # store result
-        results <- rbind(data.frame(varX, varY, mr_all_b,mr_all_p,mr_homo_b,mr_homo_p,n_obs,min_f=min(f_stat)), results)
+        # store IV-exp f-stats
+        f_stats <- rbind(f_stats, data.frame(f_stat, n_obs))
     }
 }
+
+# check f_stats
+f_stats %>% dplyr::group_by(n_obs) %>% dplyr::summarize(t.test(f_stat) %>% tidy)
+
+# plot bias
+bias <- results %>% dplyr::group_by(n_obs, q) %>% dplyr::summarize(t.test(b) %>% tidy)
+ggplot(bias, aes(x=q, y=estimate, ymin=conf.low, ymax=conf.high, shape=n_obs)) +
+    geom_point() +
+    geom_errorbar() +
+    coord_flip()
