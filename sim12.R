e1ba0bb2f21a692db116ceacab123cc1c16dcab9
Matt Lyon <ml18692@bristol.ac.uk>
2022-04-13 11:50:16 +0100

    updated sim12

--- sim12.R
+++ sim12.R
@@ -7,7 +7,7 @@ library("viridis")
 source("funs.R")
 set.seed(123)
 
-mr <- function(b_exp, b_out, se_exp, se_out, phi_p, f, q){
+mr <- function(b_exp, b_out, se_exp, se_out, phi_p, q){
     # define P-threshold using quantile
     p_var <- quantile(phi_p, q)
 
@@ -21,22 +21,16 @@ mr <- function(b_exp, b_out, se_exp, se_out, phi_p, f, q){
     ivw <- mr_ivw(b_exp, b_out, se_exp, se_out, NULL)
     ivw$q <- q
 
-    # IV-exposure F-statistics
-    f <- f[phi_p >= p_var]
-    ft <- t.test(f) %>% tidy %>% dplyr::select(estimate, conf.low, conf.high) %>% dplyr::rename(estimate.f="estimate", conf.low.f="conf.low", conf.high.f="conf.high")
-    ivw <- cbind(ivw, ft)
-
     return(ivw)
 }
 
-n_obs <- 10000
+n_obs <- 100000
 n_sim <- 50
 r2_u <- 0.2 # U-Y main effect (small Cohen d)
 r2_x <- 0.2 # X-Y main effect (small Cohen d)
 r2_xu <- r2_x * 0.5 # X-Y interaction effect half the size of the main effect
-n_snps <- 10 # number of SNPs for use as IVs in MR
-n_isnps <- 5 # number of IVs with interaction effects on X
-r2_z <- 0.05 # combined variance explained by the IV
+n_snps <- 6 # number of SNPs for use as IVs in MR
+n_isnps <- 3 # number of IVs with interaction effects on X
 phi <- 0.5 # interaction effect size relative to main effect
 x_b <- sqrt(r2_x)
 u_b <- sqrt(r2_u)
@@ -45,7 +39,7 @@ xu_b <- sqrt(r2_xu)
 results <- data.frame()
 for (i in 1:n_sim){
     # select SNP betas
-    z_b <- runif(n_snps, min = 0.01, max = 0.05)
+    z_b <- runif(n_snps, min = 0.02, max = 0.06)
     
     # set size of interaction effect relative to main effect
     zu_b <- z_b * phi
@@ -57,6 +51,7 @@ for (i in 1:n_sim){
     y <- x*x_b + u*u_b + x*u*xu_b + rnorm(n_obs, sd=sqrt(1-(r2_x+r2_u+r2_xu))) # outcome
     varX <- var(x)
     varY <- var(y)
+    r2 <- summary(lm(x~z))$r.squared
 
     # SNP estimates
     b_exp <- sapply(1:n_snps, function(i) lm(x~z[,i]) %>% tidy %>% dplyr::filter(term!="(Intercept)") %>% dplyr::pull(estimate))
@@ -67,32 +62,44 @@ for (i in 1:n_sim){
     phi_p <- sapply(1:n_snps, function(i) model(data.frame(z, x), paste0("X", i), "x")$phi_p)
 
     # estimate IVW with subsets of instruemnts
-    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, f_stat, 0.75))
-    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, f_stat, 0.5))
-    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, f_stat, 0.25))
-    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, f_stat, 0.1))
-    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, f_stat, 0.05))
-    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, f_stat, 0))
+    oracle <- mr_ivw(b_exp[4:6], b_out[4:6], se_exp[4:6], se_out[4:6], NULL)
+    oracle$q <- NA
+    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, 0.75))
+    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, 0.5))
+    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, 0.25))
+    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, 0.1))
+    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, 0.05))
+    results <- rbind(results, mr(b_exp, b_out, se_exp, se_out, phi_p, 0))
+    results <- rbind(results, oracle)
 }
 
-# plot bias
-bias <- results %>% dplyr::group_by(q) %>% dplyr::summarize(t.test(b, mu=x_b) %>% tidy)
-bias$q <- factor(bias$q)
-bias$q <- factor(bias$q, levels = rev(levels(bias$q)))
-#pdf("cont.pdf")
-ggplot(bias, aes(x=q, y=estimate, ymin=conf.low, ymax=conf.high)) +
+# plot bias and efficiency
+bias <- results %>% dplyr::group_by(q) %>% dplyr::summarize(t.test(b) %>% tidy) %>% dplyr::mutate(estimand="b")
+bias$yintercept <- x_b
+efficiency <- results %>% dplyr::group_by(q) %>% dplyr::mutate(pval=-log10(pval)) %>% dplyr::summarize(t.test(pval) %>% tidy) %>% dplyr::mutate(estimand="p")
+efficiency$yintercept <- NA
+s <- rbind(bias, efficiency)
+s$q <- as.character(s$q)
+s$q[is.na(s$q)] <- "Oracle"
+s$q <- factor(s$q)
+s$q <- factor(s$q, levels = rev(levels(s$q)))
+pdf("sim12.pdf")
+ggplot(s, aes(x=q, y=estimate, ymin=conf.low, ymax=conf.high)) +
     geom_point() +
     geom_errorbar(width=0.3) +
     coord_flip() +
-    geom_hline(yintercept=x_b, linetype="dashed", color="grey") +
-    labs(y="SD (95% CI)", x="Proportion of top instrument-variance effects removed") +
+    geom_hline(data=s, aes(yintercept = yintercept), linetype="dashed", color="grey") +
+    facet_grid(. ~ estimand, scales="free", switch = 'x', labeller = as_labeller(c(b="SD (95% CI)", p="-log10(P) (95% CI)"))) +
+    labs(x="Proportion of top instrument-variance effects removed") +
     theme_classic() +
     theme(
         strip.background = element_blank(),
         strip.text.y = element_text(angle = 0),
+        strip.placement = "outside",
         legend.position = "bottom",
         legend.background = element_blank(),
+        axis.title.x=element_blank(),
         legend.box.background = element_rect(colour = "black"),
         panel.spacing = unit(1, "lines")
     )
-#dev.off()
+dev.off()
